b836e0b62c99a313eef3cc2a4de41e647947fdc0
Yulia Ermolaeva <168757304+yuliakorsak@users.noreply.github.com>
2025-03-06 22:12:27 +0300

    redirect fix

--- exit.php
+++ exit.php
@@ -3,4 +3,4 @@ session_start();
 if(isset($_SESSION['username'])) {
     unset($_SESSION['username']);
 }
-header('Location: /index.php');
+header('Location: .');
--- post.php
+++ post.php
@@ -1,4 +1,4 @@
 <?php
 session_start();
 $_SESSION['username'] = $_POST['username'];
-header('Location: /index.php');
+header('Location: .');
